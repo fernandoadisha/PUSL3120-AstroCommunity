d1a58aa0d4f826a1b4c86672ad579e9c80775b30
Adhisha Fernando <fernandoadisha007@gmail.com>
2023-01-09 23:51:43 +0530

    Progress wiht Login and remove Toastr service functionality

--- frontend/src/app/shared/constants/urls.ts
+++ frontend/src/app/shared/constants/urls.ts
@@ -1,7 +1,9 @@
-const BASE_URL = 'http://localhost:9000/shopitem'
+const BASE_URL = 'http://localhost:9000'
 
-export const ITEMS_URL = BASE_URL + '/api/items';
+export const ITEMS_URL = BASE_URL + '/shopitem/api/items';
 export const ITEMS_TAGS_URL = ITEMS_URL + '/tags';
 export const ITEMS_BY_SEARCH_URL = ITEMS_URL + '/search/';
 export const ITEMS_BY_TAG_URL = ITEMS_URL + '/tags/';
 export const ITEMS_BY_ID = ITEMS_URL + '/';
+
+export const USER_LOGIN_URL = BASE_URL + 'user/api/login'
---
+++ frontend/src/app/shared/models/User.ts
@@ -0,0 +1,8 @@
+export class User{
+  id!:string;
+  eamil!:string;
+  name!:string;
+  address!:string;
+  token!:string;
+  isAdmin!:boolean;
+}
