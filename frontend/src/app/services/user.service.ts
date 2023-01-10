c5c70656795d81b408e22824436cea8f4362e984
Adhisha Fernando <fernandoadisha007@gmail.com>
2023-01-10 16:59:00 +0530

    Completed login functionality with some UI changes with Login

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -6,11 +6,13 @@ import { USER_LOGIN_URL } from '../shared/constants/urls';
 import { IUserLogin } from '../shared/interfaces/IUserLogin';
 import { User } from '../shared/models/User';
 
+
+const USER_KEY = 'User';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private userSubject = new BehaviorSubject<User>(new User());
+  private userSubject = new BehaviorSubject<User>(this.getUserFromLocalStorage());
   public userObservable: Observable<User>;
 
 
@@ -22,6 +24,7 @@ export class UserService {
     return this.http.post<User>(USER_LOGIN_URL, userLogin).pipe(
       tap({
         next: (user) => {
+          this.setUserToLocalStorage(user);
           this.userSubject.next(user); // on sucessful login
           /*
           this.toastrService.success(
@@ -40,4 +43,28 @@ export class UserService {
       })
     )
   }
+
+  logout() {
+    this.userSubject.next(new User());
+    localStorage.removeItem(USER_KEY);
+    window.location.reload();
+  }
+
+  private setUserToLocalStorage(user:User) {
+    localStorage.setItem(USER_KEY, JSON.stringify(user))
+  }
+
+  private getUserFromLocalStorage():User{
+    const userJson = localStorage.getItem(USER_KEY);
+    if(userJson) {
+      return JSON.parse(userJson) as User;
+    }
+    else {
+    return new User()
+    }
+  }
+
+
+
+
 }
