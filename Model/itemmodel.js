1cb050eba8fa6785c843f68339b2651361e3615b
Adhisha Fernando <fernandoadisha007@gmail.com>
2023-01-12 23:08:24 +0530

    Fixed item search, item page functionality

--- Model/itemmodel.js
+++ Model/itemmodel.js
@@ -2,12 +2,12 @@
 const mongoose = require("mongoose");
 
 const shopItemSchema = new mongoose.Schema({
-    id: {type: String},
+    //id: {type: String},
     name: {type: String},
     price: {type: Number},
     tag: {type: Array},
     imageUrl: {type: String},
     stars: {type: Number}
-});
+},{toJSON:{virtuals: true}, toObject: {virtuals: true}} );
 
 module.exports = mongoose.model("shopitems", shopItemSchema);
